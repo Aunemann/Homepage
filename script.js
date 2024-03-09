ef3ed0a63c86f72d91ab12c78e886154ea709143
Aunemann <152209611+Aunemann@users.noreply.github.com>
2024-03-09 23:00:52 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-  const button = document.querySelector('.portfolio-button');
-
-  button.addEventListener('click', function(event) {
-    event.preventDefault();
-    this.classList.add('button-animation');
-  });
-});
